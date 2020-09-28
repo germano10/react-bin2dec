d15eaf090972acce42f2e0578e8be52ca5a96160
Felipe Germano <fgermano@img.com.br>
2020-09-28 10:25:22 -0300

    finalização da função

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 const App = () => {
-  	return (
+
+	const [binario, setBinario] = useState('');
+	const [decimal, setDecimal] = useState(0);
+
+	const resultTheBin = (value) => {
+		if(value.length < 9){
+			let val = value.replace(/[2-9]/g,'')
+			let dec = 0
+
+			setBinario(val)
+
+			for(let i = val.length - 1; i >= 0; i--){
+				dec += parseInt(val[i]) * Math.pow(2, val.length - 1 - i)
+			}
+
+			setDecimal(dec)
+		}
+	}
+	
+	return (
 		<div className="container">
 			<div className="container-form">
 				<label>Digite o binário</label>
-				<input type="text" maxlength="8" minlength="0" />
+				<input onChange={(e) => resultTheBin(e.target.value)} type="number" maxLength="8" value={binario} />
+			</div>
+			<div className="container-result">
+				<span>{decimal}</span>
 			</div>
 		</div>
   	);
